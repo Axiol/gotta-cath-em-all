6d87abf2448faec1d3e932b0852f4b3f494ce8b7
Arnaud Delante <arnaud.delante@lampiris.be>
2019-06-05 13:45:56 +0200

    Split the Pokemon behaviors in a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonsModule } from './pokemons/pokemons.module';
 
-import { ListPokemonComponent } from './list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    PokemonsModule,
     AppRoutingModule
   ],
   providers: [],
